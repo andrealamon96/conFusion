916767f90a3765bc1483923814e6ead1824503d4
Andrea Lamon <andrealamon96@gmail.com>
2022-06-08 17:02:13 -0300

    create menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from "./app.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { MatListModule, MatToolbarModule } from "@angular/material";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule,
   ],
   providers: [],
